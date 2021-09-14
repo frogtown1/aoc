7e911efde8bef15145eb077037ef42a58a8898fe
frogtown1 <mcgriff.benjamin@gmail.cin>
2021-09-13 21:37:49 -0700

    Add part1() of day01

--- yr-2020/pl-erl/src/day01.erl
+++ yr-2020/pl-erl/src/day01.erl
@@ -1,10 +1,34 @@
 -module(day01).
--export([function_test/0]).
+-export([results/0]).
+
+%% === Wrapper Function =================
+results() ->
+    io:format("Part 1: ~p~n", [part1()]).
+
+%% ======================================
 
 
-function_test() ->
-    Cmd = os:cmd("ls"),
-    io:format("The result of ls is:~p~n", [Cmd]),
-    Cmd.
+
+%% === Part 1 ===========================
+part1() -> solver_part1(input_file()).
+
+solver_part1([]) -> 0;
+solver_part1([H | T]) -> solver_part1(H, T) + solver_part1(T).
+
+solver_part1(_, []) -> 0;
+solver_part1(V, [H | _]) when V + H =:= 2020 ->
+    V * H;
+solver_part1(V, [_ | T]) ->
+    solver_part1(V, T).
+
+
+
+input_file() ->
+    {ok, Input} = file:read_file(input01),
+    Strings = string:split(Input, "\n", all),
+    Pairs = [string:to_integer(X) || X <- Strings],
+    {Result, _} = lists:unzip(Pairs),
+    Result.
+
 
 
