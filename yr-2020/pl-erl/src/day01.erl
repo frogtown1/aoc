9ca5b441ceb1e0a251fd94ed8cdefddcd3c23a6a
frogtown1 <mcgriff.benjamin@gmail.cin>
2021-09-16 21:36:27 -0700

    Add part2/1

--- yr-2020/pl-erl/src/day01.erl
+++ yr-2020/pl-erl/src/day01.erl
@@ -5,13 +5,13 @@
 
 -export([results/0]).
 
+%% ===== Main =====
 -spec results() -> integer().
 results() ->
     Input = ingest_file("input01"),
-    io:format("Part 1: ~p~n", [solve_part1(Input)]).
-   % io:format("Part 2: ~p~n", [400]).
-
-
+    io:format("===== Day01 Results =====~n"),
+    io:format("Part 1: ~p~n", [solve_part1(Input)]),
+    io:format("Part 2: ~p~n", [solve_part2(Input)]).
 
 %% ==================================
 %% Part 1
@@ -30,13 +30,25 @@ results() ->
 -spec solve_part1(list()) -> integer().
 solve_part1(Input) ->
     [{Int1, Int2} | _] = [ {Int1, Int2} ||
-                         Int1 <- Input, Int2 <- Input,
-                         Int1 + Int2 =:= 2020 ],
-                     Int1 * Int2.
+                           Int1 <- Input, Int2 <- Input,
+                           Int1 + Int2 =:= 2020 ],
+    Int1 * Int2.
 
 %% ==================================
 %% Part 2
 %% ==================================
+%% Method 1: List Comprehension
+-spec solve_part2(list()) -> integer().
+solve_part2(Input) ->
+    [{Int1, Int2, Int3} | _] = [ {Int1, Int2, Int3} ||
+                                 Int1 <- Input, Int2 <- Input, Int3 <- Input,
+                                 Int1 + Int2 + Int3 =:= 2020 ],
+    Int1 * Int2 * Int3.
+
+
+
+
+
 %% L = [1721, 979, 366, 299, 675, 1465]
 
 
