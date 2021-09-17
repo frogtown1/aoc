a4da0a5c0b92c54cfaca5f4091f4de21c993c874
frogtown1 <mcgriff.benjamin@gmail.cin>
2021-09-16 21:27:26 -0700

    Add Day01-Part1-Method2

--- yr-2020/pl-erl/src/day01.erl
+++ yr-2020/pl-erl/src/day01.erl
@@ -1,35 +1,47 @@
 %% @author  Benjamin McGriff
 %% @doc     AoC Day01 Solutions.
-%% @version 0.1
 
 -module(day01).
 
--export([results/0, ingest_file/1]).
+-export([results/0]).
 
-
+-spec results() -> integer().
 results() ->
     Input = ingest_file("input01"),
-    io:format("Part 1: ~p~n", [solver_part1(Input)]).
+    io:format("Part 1: ~p~n", [solve_part1(Input)]).
    % io:format("Part 2: ~p~n", [400]).
+
 
 
 %% ==================================
 %% Part 1
-solver_part1(Input) -> solver_part1(Input, Input, Input).
+%% ==================================
+%% Method 1: Tail Recursion
+%solve_part1(Input) -> solve_part1(Input, Input, Input).
 
-solver_part1([H1 | _], [H2 | _], _) when H1 + H2 =:= 2020 ->
-    H1 * H2;
-solver_part1([], [_ | T], Input) ->
-    solver_part1(Input, T, Input);
-solver_part1([_ | T], List, Input) ->
-    solver_part1(T, List, Input).
+%solve_part([H1 | _], [H2 | _], _) when H1 + H2 =:= 2020 ->
+%    H1 * H2;
+%solve_part([], [_ | T], Input) ->
+%    solver_part(Input, T, Input);
+%solve_part1([_ | T], List, Input) ->
+%    solve_part1(T, List, Input).
 
+%% Method 2: List Comprehension
+-spec solve_part1(list()) -> integer().
+solve_part1(Input) ->
+    [{Int1, Int2} | _] = [ {Int1, Int2} ||
+                         Int1 <- Input, Int2 <- Input,
+                         Int1 + Int2 =:= 2020 ],
+                     Int1 * Int2.
 
+%% ==================================
+%% Part 2
+%% ==================================
 %% L = [1721, 979, 366, 299, 675, 1465]
 
 
 %% Ingest input file
-
+-spec ingest_file(string()) -> list().
 ingest_file(FileName) ->
     FilePath        = filename:join(["..", "resources", FileName]),
     {ok, Data}      = file:read_file(FilePath),
